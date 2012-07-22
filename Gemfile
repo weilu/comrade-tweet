2198856c55881107c84fa041126c2df4cf255cc8
Wei Lu <luwei.here@gmail.com>
2012-07-22 21:06:49 +0800

    initial commit

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.5'
+
+gem 'pg'
+gem "sass"
+gem "haml"
+gem 'unicorn'
+gem 'jquery-rails'
+gem 'omniauth-twitter'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier'
+  gem "twitter-bootstrap-rails"
+end
+
+group :development, :test do
+  gem "capybara"
+  gem "factory_girl_rails"
+  gem "rspec-rails"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ComradeTweet::Application.config.secret_token = '4a864bdd8f1885083a2ab107958cc4821b44101ab43fc6cef0ae3d0da6f21124cf2a75fce4bbff55c11203fc0f7772d4fe178d475c6b4aa663a4a618f192e511'
